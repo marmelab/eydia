247a41107708f803f753f856c19ef5a6749d6f4b
Gildas Garcia <gildas.garcia@pm.me>
2019-04-13 20:17:35 +0200

    Better method to get the project name

--- packages/cli/src/commands/get-project-name.js
+++ packages/cli/src/commands/get-project-name.js
@@ -1,9 +1,74 @@
-import { resolve } from 'path';
+import { basename, extname } from 'path';
+import execa from 'execa';
+import path from 'path';
+import meow from 'meow';
 
-const getProjectName = (projectRoot = process.cwd()) => {
-    const packageJson = require(resolve(projectRoot, 'package.json'));
+const defaultCliParams = meow({
+    flags: {
+        project: {
+            type: 'string',
+        },
+    },
+});
 
-    return packageJson.name;
+const defaultCurrentPath = process.cwd();
+
+const defaultParams = {
+    currentPath: defaultCurrentPath,
+    cliParams: defaultCliParams,
+};
+
+/*
+ * If user has specified a project using the CLI flags, use this name.
+ * If the current path (cwd) is inside a git repository, use the repository name
+ * If the current path (cwd) is not inside a git repository, use the directory name
+ */
+const getProjectName = async ({
+    currentPath = defaultCurrentPath,
+    cliParams = defaultCliParams,
+} = defaultParams) => {
+    if (cliParams.flags.project) {
+        return cliParams.flags.project;
+    }
+
+    // TODO: Detect other types of repository (mercurial, etc.)
+    const candidates = await Promise.all([
+        getProjectNameFromGitRepository(currentPath),
+        getProjectNameFromDirectory(currentPath),
+    ]);
+
+    return candidates.find(candidate => Boolean(candidate));
 };
 
 export default getProjectName;
+
+const getProjectNameFromDirectory = path => basename(path);
+
+const getFileNameWithoutExtension = name => {
+    const extension = extname(name);
+
+    return name.replace(extension, '');
+};
+
+const getProjectNameFromGitRepository = async (
+    currentPath = defaultCurrentPath
+) => {
+    const thisPath = path.isAbsolute(currentPath)
+        ? currentPath
+        : path.join(defaultCurrentPath, currentPath);
+
+    try {
+        const { stdout } = await execa(
+            'git',
+            ['config', '--get', 'remote.origin.url'],
+            {
+                cwd: thisPath,
+            }
+        );
+
+        const result = basename(stdout);
+        return getFileNameWithoutExtension(result);
+    } catch (e) {
+        return undefined;
+    }
+};
